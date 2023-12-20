9d9458af567b5341766f1c721501956b9d731922
Younnes Chebli <younnesc@gmail.com>
2023-12-20 14:27:45 +0100

    refactor: updated context

--- backend/Models/MsnContext.cs
+++ backend/Models/MsnContext.cs
@@ -30,25 +30,25 @@ public class MsnContext : DbContext
 
         modelBuilder.Entity<Member>().HasData(
             new Member { Pseudo = "admin", Password = "admin", FullName = "Admin", Role = Role.Admin },
-            new Member { Pseudo = "ben", Password = "ben", FullName = "Benoît Penelle", BirthDate = new DateTime(1970, 1, 2) },
-            new Member { Pseudo = "bruno", Password = "bruno", FullName = "Bruno Lacroix", BirthDate = new DateTime(1971, 2, 3) },
-            new Member { Pseudo = "alain", Password = "alain", FullName = "Alain Silovy" },
-            new Member { Pseudo = "xavier", Password = "xavier", FullName = "Xavier Pigeolet" },
-            new Member { Pseudo = "boris", Password = "boris", FullName = "Boris Verhaegen" },
-            new Member { Pseudo = "marc", Password = "marc", FullName = "Marc Michel" }
+            new Member { Pseudo = "harry", Password = "harry", FullName = "Harry Covère", BirthDate = new DateTime(1970, 1, 2) },
+            new Member { Pseudo = "john", Password = "john", FullName = "John Deuf", BirthDate = new DateTime(1971, 2, 3) },
+            new Member { Pseudo = "jim", Password = "jim", FullName = "Jim Nastik" },
+            new Member { Pseudo = "camille", Password = "camille", FullName = "Camille Honnête" },
+            new Member { Pseudo = "melusine", Password = "melusine", FullName = "Mélusine Enfayite" },
+            new Member { Pseudo = "alain", Password = "alain", FullName = "Alain Terrieur" }
         );
 
         modelBuilder.Entity<Phone>().HasData(
-            new Phone { PhoneId = 1, Type = "aaa", Number = "123", MemberPseudo = "ben" },
-            new Phone { PhoneId = 2, Type = "bbb", Number = "456", MemberPseudo = "ben" }
+            new Phone { PhoneId = 1, Type = "aaa", Number = "123", MemberPseudo = "harry" },
+            new Phone { PhoneId = 2, Type = "bbb", Number = "456", MemberPseudo = "harry" }
         );
 
         modelBuilder.Entity<Follow>().HasData(
-            new Follow { FollowerPseudo = "ben", FolloweePseudo = "bruno" },
-            new Follow { FollowerPseudo = "ben", FolloweePseudo = "alain" },
-            new Follow { FollowerPseudo = "bruno", FolloweePseudo = "boris" },
-            new Follow { FollowerPseudo = "bruno", FolloweePseudo = "ben" },
-            new Follow { FollowerPseudo = "bruno", FolloweePseudo = "xavier" }
+            new Follow { FollowerPseudo = "harry", FolloweePseudo = "john" },
+            new Follow { FollowerPseudo = "harry", FolloweePseudo = "alain" },
+            new Follow { FollowerPseudo = "john", FolloweePseudo = "melusine" },
+            new Follow { FollowerPseudo = "john", FolloweePseudo = "harry" },
+            new Follow { FollowerPseudo = "john", FolloweePseudo = "camille" }
         );
     }
 
